61182c45de8bd84eec2f0548c1abd4562e5a42ba
0xb1b1 <voxelflee@protonmail.com>
2023-12-10 01:45:29 +0300

    feat(backend): create add_portfolio method

--- backend/src/ds_backend/http/routers/portfolios.py
+++ backend/src/ds_backend/http/routers/portfolios.py
@@ -2,6 +2,7 @@
 from fastapi import APIRouter, Security, HTTPException
 from fastapi_jwt import JwtAuthorizationCredentials
 from loguru import logger
+import uuid
 from pymongo.errors import DuplicateKeyError
 from datetime import datetime
 from urllib import parse as urlparse
@@ -17,17 +18,24 @@ from ds_backend import config
 
 from ds_backend.db.databases import backend_db, \
     news_db
+
 # from ds_backend.db.repositories.news.article import \
 #     ArticleRepository
+
 from ds_backend.db.repositories.news.stock_info import \
     StockInfoRepository
+
 from ds_backend.db.repositories.portfolio import \
     PortfolioRepository
+
 from ds_backend.http.schemas.portfolio import \
     PortfolioSchema, \
     PortfolioStockSchema, \
     PortfolioStrippedSchema, \
     PortfoliosDashboardSchema
+
+from ds_backend.models.portfolio import \
+    Portfolio, PortfolioStock
 
 
 router = APIRouter(
@@ -42,8 +50,56 @@ stock_info_repo = StockInfoRepository(database=news_db)
 portfolio_repo = PortfolioRepository(database=backend_db)
 
 
+@router.post(
+    "/add_portfolio",
+    response_model=Portfolio,
+)
+def add_portfolio(
+    portfolio: PortfolioSchema,
+    credentials: JwtAuthorizationCredentials = Security(
+        config.jwt_ac,
+    ),
+):
+    portfolio_uuid = str(uuid.uuid4())
+    portfolio_stocks: list[PortfolioStock] = []
+    for stock in portfolio.stocks:
+        portfolio_stocks.append(
+            PortfolioStock(
+                ticker=stock.ticker,
+                amount=stock.amount,
+                is_paused=stock.is_paused,
+                is_disabled=stock.is_disabled,
+            )
+        )
+
+    portfolio_repo.save(
+        Portfolio(
+            uuid=portfolio_uuid,
+            owner_email=credentials["email"],
+            sector=portfolio.sector,
+            balance=portfolio.balance,
+            stocks=portfolio_stocks,
+        )
+    )
+
+    db_portfolio = portfolio_repo.find_one_by(
+        {"uuid": portfolio_uuid}
+    )
+
+    if db_portfolio is None:
+        logger.error(
+            f"Failed to find added portfolio with UUID {portfolio_uuid} ({portfolio})"  # noqa: E501
+        )
+        raise HTTPException(
+            status_code=500,
+            detail="Failed to find the portfolio in the database"
+        )
+
+    return db_portfolio
+
+
 @router.get(
-    "/get_all",
+    "/get_all_portfolios",
     response_model=PortfoliosDashboardSchema,
 )
 def get_all_portfolios(
