50b38b1ad38c889ba57db3d3ba800be22b57737d
0xb1b1 <voxelflee@protonmail.com>
2023-12-10 02:53:10 +0300

    util(backend): add unique constraint (index) on owner_email+sector

--- backend/src/ds_backend/db/repositories/portfolio.py
+++ backend/src/ds_backend/db/repositories/portfolio.py
@@ -1,5 +1,6 @@
 from pydantic_mongo import AbstractRepository
 from pymongo.database import Database
+from pymongo import ASCENDING
 
 from ds_backend.models.portfolio import \
     Portfolio
@@ -8,8 +9,18 @@ from ds_backend.models.portfolio import \
 class PortfolioRepository(AbstractRepository[Portfolio]):
     def __init__(self, database: Database):
         AbstractRepository.__init__(self, database)
+
+        # UUIDs should be unique
         database["portfolios"].create_index("uuid", unique=True)
-        # TODO: Create index on owner+category?
+
+        # Only one portfolio in one sector inside a user's account
+        database["portfolios"].create_index(
+            [
+                ("owner_email", ASCENDING),
+                ("sector", ASCENDING)
+            ],
+            unique=True
+        )
 
     class Meta:
         collection_name = "portfolios"
