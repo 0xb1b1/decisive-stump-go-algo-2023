f839b4dfa6b6d0eafea5aa1af7583ac85a334ff4
0xb1b1 <voxelflee@protonmail.com>
2023-12-09 05:34:19 +0300

    util(backend): add initial models for portfolios, add sorting to news

---
+++ backend/src/ds_backend/http/schemas/user_portfolio.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+from pydantic import BaseModel, Field
+# from enum import Enum
+
+
+class PortfolioStockSchema(BaseModel):
+    ticker: str = Field(
+        description="Stock ticker. Example: YNDX.",
+    )
+
+    amount: int = Field(
+        default=0,
+        description="Amount of this stock in the portfolio.",
+    )
+
+    is_paused: bool = Field(
+        default=False,
+        description="TODO",
+    )
+
+    is_disabled: bool = Field(
+        default=False,
+        description="TODO",
+    )
+
+
+class PortfolioSchema(BaseModel):
+    sector: str = Field(
+        description="Portfolio name/sector."
+    )
+
+    balance: float = Field(
+        default=0.0,
+        description="Available money in the portfolio."
+    )
+
+    # class Config:
+    #     use_enum_values = True
